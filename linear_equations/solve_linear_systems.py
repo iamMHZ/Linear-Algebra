5898380315984d922107e0860e747456c6117775
Mohammad Hossein <iammhz77@gmail.com>
2020-04-14 17:42:39 +0430

    Solving Systems of Linear Equations with Numpy

--- linear_equations/solve_linear_systems.py
+++ linear_equations/solve_linear_systems.py
@@ -8,7 +8,7 @@ b = np.array([7, -16, -40])
 x = np.linalg.solve(A, b)
 pprint.pprint(x)
 
-# below code rises exception for solving linear equation , because the determinant of matrix A is zero
+# below code rises an exception for the solving linear equation , because the determinant of the  matrix A is zero
 
 A = np.array([[3, 5, -4], [-3, -2, 4], [6, 1, -8]])
 determinant = np.linalg.det(A)
